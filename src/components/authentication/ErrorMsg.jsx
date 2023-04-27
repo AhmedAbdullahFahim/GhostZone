773b960d6cef36eabe6b1f66a353d627921c5515
ahmedabdullahfahim <ahmed.abdallah5022@gmail.com>
2023-04-27 22:01:09 +0200

    adjusted error messages styling

--- src/components/authentication/ErrorMsg.jsx
+++ src/components/authentication/ErrorMsg.jsx
@@ -1,8 +1,14 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import Error from '../../assets/images/error.svg';
 
 const ErrorMsg = ({message}) => {
-  return <Text className='text-sm font-bold text-red-500 mb-5'>{message}</Text>;
+  return (
+    <View className="flex-row items-center mb-5">
+      <Error />
+      <Text className="text-sm font-bold text-[#AC3F3B] ml-1">{message}!</Text>
+    </View>
+  );
 };
 
 export default ErrorMsg;
